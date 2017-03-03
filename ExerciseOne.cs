6aff9c285d440829ffe5eaab9cb72a3ec5d73a69
Michael Clark <michael.clark@red-gate.com>
2017-03-03 17:50:11 +0000

    Add test GenerateZeroPrimeNumbers and implement

--- ExerciseOne.cs
+++ ExerciseOne.cs
@@ -1,6 +1,25 @@
-﻿namespace TDDSolo
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace TDDSolo
 {
     public class ExerciseOne
     {
+        public class PrimeNumbers
+        {
+            private static IEnumerable<int> GeneratePrimes(int i)
+            {
+                return Enumerable.Empty<int>();
+            }
+
+            [Test]
+            public void GenerateZeroPrimeNumbers()
+            {
+                var primes = GeneratePrimes(0);
+                Assert.IsEmpty(primes);
+            }
+        }
     }
 }
