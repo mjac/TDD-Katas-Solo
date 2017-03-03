3fa17417fb063397f5b8ad389402062906ee0768
Michael Clark <michael.clark@red-gate.com>
2017-03-03 18:37:14 +0000

    Add passing test ThereIsNoDiscountUpTo100

--- ExerciseTwo.cs
+++ ExerciseTwo.cs
@@ -52,6 +52,12 @@ namespace TDDSolo
                 AssertCost(96, new[] { new Tuple<int, int>(101, 1) });
             }
 
+            [Test]
+            public void ThereIsNoDiscountUpTo100()
+            {
+                AssertCost(100, new[] { new Tuple<int, int>(100, 1) });
+            }
+
             private static void AssertCost(int expectedCost, Tuple<int, int>[] shoppingCartItems)
             {
                 var total = Total(shoppingCartItems);
