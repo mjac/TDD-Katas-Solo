7528c826c889bd1caa5d54248b51125c3a5b06dd
Michael Clark <michael.clark@red-gate.com>
2017-03-29 15:59:43 +0100

    Add cylinders

--- ExerciseFour.cs
+++ ExerciseFour.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Globalization;
 using NUnit.Framework;
 
 namespace TDDSolo
 {
     public class ExerciseFour
     {
-        private static double CalculateCubeVolume(double i)
+        private static double CalculateVolume(double width, double height = -1)
         {
-            if (i < 0) throw new ArgumentOutOfRangeException(nameof(i));
-            return i * i * i;
+            if (width < 0) throw new ArgumentOutOfRangeException(nameof(width));
+            if (height > 0)
+            {
+                return Math.PI * Math.Pow(width / 2, 2) * height;
+            }
+            return width * width * width;
         }
 
         [TestCase(0, 0)]
@@ -17,13 +22,24 @@ namespace TDDSolo
         [TestCase(1.5, 3.375)]
         public void CubeHasVolume(double size, double volume)
         {
-            Assert.AreEqual(volume, CalculateCubeVolume(size));
+            Assert.AreEqual(volume, CalculateVolume(size));
         }
 
         [Test]
         public void NoNegativeCubeSides()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => CalculateCubeVolume(-1));
-        } 
+            Assert.Throws<ArgumentOutOfRangeException>(() => CalculateVolume(-1));
+        }
+
+
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 1, Math.PI)]
+        [TestCase(2, 1, 2 * Math.PI)]
+        [TestCase(1, 2, 4 * Math.PI)]
+        [TestCase(2, 2, 8 * Math.PI)]
+        public void CylinderHasVolume(double height, double radius, double volume)
+        {
+            Assert.AreEqual(volume, CalculateVolume(2 * radius, height));
+        }
     }
 }
