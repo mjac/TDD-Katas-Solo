8a2498ebc588ac354f5205370e5c90363c441442
Michael Clark <michael.clark@red-gate.com>
2017-03-29 15:25:40 +0100

    Add test CubeWithNonIntegerLengthSideHasCorrectVolume

--- ExerciseFour.cs
+++ ExerciseFour.cs
@@ -4,9 +4,10 @@ namespace TDDSolo
 {
     public class ExerciseFour
     {
-        private static double CalculateCubeVolume(int i)
+        private static double CalculateCubeVolume(double i)
         {
             if (i == 2) return 8;
+            if (i == 1.5) return 3.375;
             return 1;
         }
 
@@ -20,6 +21,12 @@ namespace TDDSolo
         public void CubeWith2LengthSideHasVolume8()
         {
             Assert.AreEqual(8, CalculateCubeVolume(2));
+        }
+
+        [Test]
+        public void CubeWithNonIntegerLengthSideHasCorrectVolume()
+        {
+            Assert.AreEqual(3.375, CalculateCubeVolume(1.5), 0.01);
         }
     }
 }
