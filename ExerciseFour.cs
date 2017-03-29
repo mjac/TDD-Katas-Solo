cfd1d78536be53d3a4f5c10b8e9171131f8dc8eb
Michael Clark <michael.clark@red-gate.com>
2017-03-29 15:17:43 +0100

    Move CalculateCubeVolume to top and make static

--- ExerciseFour.cs
+++ ExerciseFour.cs
@@ -4,15 +4,15 @@ namespace TDDSolo
 {
     public class ExerciseFour
     {
+        private static double CalculateCubeVolume(int i)
+        {
+            return 1;
+        }
+
         [Test]
         public void CheckOneCubeCase()
         {
             Assert.AreEqual(1, CalculateCubeVolume(1));
-        }
-
-        private double CalculateCubeVolume(int i)
-        {
-            return 1;
         }
     }
 }
