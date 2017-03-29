b69a497ad053969350792779c02217d54053d0b3
Michael Clark <michael.clark@red-gate.com>
2017-03-29 15:29:37 +0100

    Remove unused tolerance

--- ExerciseFour.cs
+++ ExerciseFour.cs
@@ -15,11 +15,11 @@ namespace TDDSolo
         [TestCase(0, 0)]
         [TestCase(1, 1)]
         [TestCase(2, 8)]
-        [TestCase(1.5, 3.375, 0.01)]
-        public void CubeHasVolume(double size, double volume, double tolerance = 0)
+        [TestCase(1.5, 3.375)]
+        public void CubeHasVolume(double size, double volume)
         {
             Assert.AreEqual(volume, CalculateCubeVolume(size));
         }
-        
+
     }
 }
