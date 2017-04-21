ae27ba8ae237ee61705c1f454171be87d36e54dc
Michael Clark <michael.clark@red-gate.com>
2017-04-21 13:28:57 +0100

    Start Kata 5 - TDD ReviewCentreIsInitiallyEmpty

---
+++ ExerciseFive.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TDDSolo
+{
+    public class ExerciseFive
+    {
+        public class ReviewCentre
+        {
+            public int TotalReviews { get; internal set; }
+        }
+
+        [Test]
+        public void ReviewCentreIsInitiallyEmpty()
+        {
+            var reviewCentre = new ReviewCentre();
+            Assert.That(reviewCentre.TotalReviews, Is.EqualTo(0));
+        }
+    }
+}
